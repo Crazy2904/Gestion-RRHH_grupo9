71c809f1cf24c48c664de517d5001194be00f8de
leydisamanez <leydisamanez257@gmail.com>
2025-11-29 21:46:54 -0500

    repositoryHoras

---
+++ src/main/java/com/example/gestionrrhh/repository/HorasExtraRepository.java
@@ -0,0 +1,7 @@
+package com.example.gestionrrhh.repository;
+
+import com.example.gestionrrhh.entity.HorasExtra;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HorasExtraRepository extends JpaRepository<HorasExtra, Long> {
+}
