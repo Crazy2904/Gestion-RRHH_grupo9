bf4e24abfeb3cb9f896cb630b011ecd97a1e8d8c
Crazy2904 <dalonsozc@gmail.com>
2025-11-30 18:58:05 -0500

    Revert "Merge branch 'dev' into diego"
    This reverts commit fbd32149425e8ca43648b76176fa82c36c36cb29, reversing
    changes made to eff1bd3224b299aeccb6431a2fdf47cc128b0f40.

--- src/main/java/com/example/gestionrrhh/repository/PermisoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.gestionrrhh.repository;
-
-import com.example.gestionrrhh.entity.Permiso;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PermisoRepository extends JpaRepository<Permiso, Long> {
-}
