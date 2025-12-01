2c49a105af5259072cdd6a43e19499d9d59b2b00
Crazy2904 <dalonsozc@gmail.com>
2025-11-30 19:36:15 -0500

    Joaquin
    * entityPermiso
    
    * repositoryPermiso
    
    * servicePermiso
    
    * controllerPermiso
    
    * controllerPermiso
    
    ---------
    
    Co-authored-by: Aramyz12 <castigadorjlr@gmail.com>

---
+++ src/main/java/com/example/gestionrrhh/repository/PermisoRepository.java
@@ -0,0 +1,7 @@
+package com.example.gestionrrhh.repository;
+
+import com.example.gestionrrhh.entity.Permiso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PermisoRepository extends JpaRepository<Permiso, Long> {
+}
